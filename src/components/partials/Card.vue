310e3b21c944eabc22c0a35f4450041de6d1be52
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-02 18:20:33 +0200

    add .card and .col

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -5,9 +5,25 @@
 </script>
 
 <template>
-  CARD
+  <div class="col">
+    <div class="card">
+      <img src="" alt="img">
+      <h3>NAME</h3>
+      <span>TYPE</span>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+  @use '../../assets/scss/general/var' as *;
+  
+  .col {
+    padding: 20px;
+    
+    .card {
+      width: calc(100% / 5);
+      background-color: $primary-color;
+      text-align: center;
+    }
+  }
 </style>
